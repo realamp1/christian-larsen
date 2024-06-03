7127c72b3b2993e96650c485f1d04d279b861673
Christian Larsen <christi9nl9rsen@gmail.com>
2024-06-03 14:51:25 -0500

    yer

---
+++ index.js
@@ -0,0 +1,15 @@
+function tracker(){
+    window.location.href("https://tracker.gg/");
+};
+
+function photography(){
+    window.location.href("#");
+};
+
+function editing(){
+    window.location.href("#");
+};
+
+function youtube(){
+    window.location.href("https://youtube.com/@realkairos");
+};
